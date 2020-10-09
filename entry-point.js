f7cf6e27286d845666439722a8ff425ac8b5d2b4
Lcnvdl <programando.soft@gmail.com>
2020-10-09 15:48:16 -0300

    Lint

--- entry-point.js
+++ entry-point.js
@@ -1,48 +1,53 @@
+const path = require("path");
+
 module.exports = ({ CommandBase, commands }) => {
-    const path = require("path");
-    const scripts = path.join(__dirname, "scripts");
+  const scripts = path.join(__dirname, "scripts");
 
-    class LintRulesCommand extends CommandBase {
-        async run(args) {
-            if (args[0] === "js") {
-                if (args[1] === "init") {
-                    return await this._initializeJs();
-                }
-                else if (args[1] === "update") {
-                    await this._update();
-                    return await this._copyEslintFile();
-                }
-                else if (args[1] === "copy") {
-                    return await this._copyEslintFile();
-                }
-            }
+  const PipeCommand = commands.pipe;
 
-            return this.codes.invalidArguments;
+  class LintRulesCommand extends CommandBase {
+    async run(args) {
+      if (args[0] === "js") {
+        if (args[1] === "init") {
+          return await this._initializeJs();
         }
 
-        async _initializeJs() {
-            const env = this.environment.fork();
-            const pipe = new (commands.pipe)(env);
-            env.setVariable("lint-rules-dir", __dirname);
-            await pipe.loadFromFile(path.join(scripts, "eslint-install.cmd"));
-            return this.codes.success;
+        if (args[1] === "update") {
+          await this._update();
+          return await this._copyEslintFile();
         }
 
-        async _update() {
-            const env = this.environment.fork(__dirname);
-            const pipe = new (commands.pipe)(env);
-            await pipe.loadFromFile(path.join(scripts, "repo-update.cmd"));
-            return this.codes.success;
+        if (args[1] === "copy") {
+          return await this._copyEslintFile();
         }
+      }
 
-        async _copyEslintFile() {
-            const env = this.environment.fork();
-            const pipe = new (commands.pipe)(env);
-            env.setVariable("lint-rules-dir", __dirname);
-            await pipe.loadFromFile(path.join(scripts, "eslint-copy.cmd"));
-            return this.codes.success;
-        }
+      return this.codes.invalidArguments;
     }
 
-    return LintRulesCommand;
+    async _initializeJs() {
+      const env = this.environment.fork();
+      const pipe = new PipeCommand(env);
+      env.setVariable("lint-rules-dir", __dirname);
+      await pipe.loadFromFile(path.join(scripts, "eslint-install.cmd"));
+      return this.codes.success;
+    }
+
+    async _update() {
+      const env = this.environment.fork(__dirname);
+      const pipe = new PipeCommand(env);
+      await pipe.loadFromFile(path.join(scripts, "repo-update.cmd"));
+      return this.codes.success;
+    }
+
+    async _copyEslintFile() {
+      const env = this.environment.fork();
+      const pipe = new PipeCommand(env);
+      env.setVariable("lint-rules-dir", __dirname);
+      await pipe.loadFromFile(path.join(scripts, "eslint-copy.cmd"));
+      return this.codes.success;
+    }
+  }
+
+  return LintRulesCommand;
 };
