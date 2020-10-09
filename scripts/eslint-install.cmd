921ce2efabb66e9278d0349be7e12d8e3480c32e
Lcnvdl <programando.soft@gmail.com>
2020-10-09 15:42:11 -0300

    Lint: compatibility with chai

--- scripts/eslint-install.cmd
+++ scripts/eslint-install.cmd
@@ -4,6 +4,9 @@ npm i --save-dev eslint
 echo Installing ESLint Airbnb style...
 npm i --save-dev eslint-plugin-import eslint-config-airbnb-base
 
+echo Installing ESLint Chai plugin...
+npm i eslint-plugin-chai-friendly --save-dev
+
 Echo Copying Json...
 copy "[lint-rules-dir]/js/.eslintrc.json" ".eslintrc.json"
 
